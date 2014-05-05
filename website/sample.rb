00bc2de413e1c8f06e41eec9dbbd9b0ba80a1389
Sam <sam.saffron@gmail.com>
2014-05-05 17:24:17 +1000

    Auto load samples

--- website/sample.rb
+++ website/sample.rb
@@ -3,19 +3,58 @@ require 'logster'
 require 'logster/middleware/reporter'
 require 'logster/middleware/viewer'
 require 'logster/redis_store'
+require 'json'
 
 require 'sinatra'
 require 'sinatra/base'
 
 # log a few errors
-$store = Logster::RedisStore.new
-
+$redis = Redis.new
+$store = Logster::RedisStore.new($redis)
 $log = Logster::Logger.new($store)
 
-$log.info "Some info line"
-$log.warn "Some warn line"
-$log.error "Some error line"
-$log.debug "Some debug line"
+class SampleLoader
+  def initialize
+    @index = 0
+    @sample_data_key = 'sample_data'
+  end
+
+
+  def ensure_samples_loaded
+    $redis.del @sample_data_key
+    data = File.read('data/data.json')
+    parsed = JSON.parse(data)
+    parsed.each do |row|
+      $redis.rpush @sample_data_key, JSON.fast_generate(row)
+    end
+    @length = parsed.length
+  end
+
+  def load_samples
+    Thread.new do
+      while true
+        sleep 5
+        begin
+          load_next_sample
+        rescue => e
+          $store.report(4, "logster", e.to_s)
+        end
+      end
+    end
+  end
+
+  def load_next_sample
+    message =  JSON.parse($redis.lindex(@sample_data_key, @index))
+    @index += 1
+    @index %= @length
+
+    $store.report(message["severity"], message["progname"], message["message"])
+  end
+end
+
+$loader = SampleLoader.new
+$loader.ensure_samples_loaded
+$loader.load_samples
 
 class Sample < Sinatra::Base
   use Logster::Middleware::Reporter, log: $log
