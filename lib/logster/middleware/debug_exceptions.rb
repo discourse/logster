d74bdd1d09848eba6ae0b4063e92f8463c2eb456
Guo Xiang Tan <tgx_world@hotmail.com>
2017-10-27 12:06:39 +0800

    Fix `Logster::Middleware::DebugExceptions` error with Rails 5.
    fixes https://github.com/discourse/logster/issues/55

--- lib/logster/middleware/debug_exceptions.rb
+++ lib/logster/middleware/debug_exceptions.rb
@@ -1,7 +1,14 @@
 class Logster::Middleware::DebugExceptions < ActionDispatch::DebugExceptions
   private
 
-  def log_error(env,wrapper)
+  def log_error(request_or_env, wrapper)
+    env =
+      if Rails::VERSION::MAJOR > 4
+        request_or_env.env
+      else
+        request_or_env
+      end
+
     exception = wrapper.exception
 
     Logster.config.current_context.call(env) do
