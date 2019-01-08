b2ed15d524dfa610d409b471288b7f2b3eedb875
Osama Sayegh <asooomaasoooma90@gmail.com>
2019-01-08 22:59:40 +0300

    Apply prettier

--- client-app/app/components/env-tab.js
+++ client-app/app/components/env-tab.js
@@ -1,4 +1,4 @@
-import Component from '@ember/component';
+import Component from "@ember/component";
 import { computed } from "@ember/object";
 import { buildHashString } from "client-app/lib/utilities";
 
