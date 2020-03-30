0d43644030281100031ced075c1d6a04ed6f98bb
Osama Sayegh <asooomaasoooma90@gmail.com>
2020-03-30 06:56:35 +0300

    FEATURE: Upgrade to Font Awesome 5 (#113)
    * FEATURE: Upgrade to Font Awesome 5
    
    * Adjust buttons CSS

---
+++ client-app/config/icons.js
@@ -0,0 +1,26 @@
+module.exports = function() {
+  return {
+    "free-solid-svg-icons": [
+      "check-square",
+      "trash-alt",
+      "ellipsis-h",
+      "share",
+      "external-link-square-alt",
+      "fast-backward",
+      "backward",
+      "fast-forward",
+      "forward",
+      "plus",
+      "check",
+      "redo-alt",
+      "exclamation-circle",
+      "times-circle",
+      "question-circle",
+      "cog",
+      "lock",
+      "unlock"
+    ],
+    "free-regular-svg-icons": ["trash-alt", "check-square", "copy", "clone"]
+    // "free-brands-svg-icons": []
+  };
+};
