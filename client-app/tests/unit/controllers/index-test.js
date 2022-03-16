203bc9393ce1c19e48a4bd97180b31b2b1034a53
Nat <natalie.tay@discourse.org>
2022-03-17 01:22:15 +0800

    Use search query param as filter

--- client-app/tests/unit/controllers/index-test.js
+++ client-app/tests/unit/controllers/index-test.js
@@ -1,12 +1,36 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import { default as MessageCollection } from "client-app/models/message-collection";
+import sinon from "sinon";
+import * as utilities from "client-app/lib/utilities";
 
-module('Unit | Controller | index', function(hooks) {
+module('Unit | Controller | index', function (hooks) {
   setupTest(hooks);
 
   // Replace this with your real tests.
-  test('it exists', function(assert) {
+  test('it exists', function (assert) {
     let controller = this.owner.lookup('controller:index');
     assert.ok(controller);
+  });
+
+  test('uses search param to filter results', function (assert) {
+    const controller = this.owner.lookup('controller:index');
+    const ajaxStub = sinon.stub(utilities, 'ajax')
+    const messages = MessageCollection.create();
+    const row1 = {"message": "error tomtom", "severity": 2, "key": "ce1f53b0cc"};
+    const row2 = {"message": "error steaky", "severity": 3, "key": "b083352825"};
+
+    messages.rows.addObjects([row1, row2]);
+    controller.set("model", messages)
+
+    assert.equal(controller.searchTerm, null, 'initial value is null');
+    assert.deepEqual(controller.model.rows, [row1, row2], 'all rows');
+
+    ajaxStub.callsFake(() => Promise.resolve({search: "tomtom", filter: [5], messages: []}));
+    controller.set("search", "tomtom");
+
+    assert.equal(controller.searchTerm, "tomtom", 'search sets search term');
+    assert.equal(ajaxStub.firstCall.args[0], "/messages.json", "get messages");
+    assert.deepEqual(ajaxStub.firstCall.args[1], {"data": {"filter": "5", "search": "tomtom"}}, "with correct terms");
   });
 });
