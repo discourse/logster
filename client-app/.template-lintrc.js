b9cc13eb89f41194658cd11457008ad0314331a7
Jarek Radosz <jradosz@gmail.com>
2022-11-08 18:35:32 +0100

    DEV: Disable new linting rules (js, hbs)

--- client-app/.template-lintrc.js
+++ client-app/.template-lintrc.js
@@ -2,4 +2,16 @@
 
 module.exports = {
   extends: "recommended",
+  rules: {
+    "no-action": "off",
+    "no-curly-component-invocation": "off",
+    "no-implicit-this": "off",
+    "no-invalid-interactive": "off",
+    "no-link-to-positional-params": "off",
+    "no-quoteless-attributes": "off",
+    "no-triple-curlies": "off",
+    "no-unbound": "off",
+    "require-input-label": "off",
+    "require-valid-alt-text": "off",
+  },
 };
