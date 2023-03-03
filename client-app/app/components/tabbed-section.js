40d7c7338e15ebf89d6fdda8d797ef9665d816d6
Jarek Radosz <jradosz@gmail.com>
2023-03-03 12:52:16 +0100

    DEV: Fix message-info actions

--- client-app/app/components/tabbed-section.js
+++ client-app/app/components/tabbed-section.js
@@ -1,11 +1,14 @@
 import classic from "ember-classic-decorator";
 import Component from "@ember/component";
 import { A } from "@ember/array";
+import { action } from "@ember/object";
 
 @classic
 export default class TabbedSection extends Component {
   tabs = A();
+  selected = null;
 
+  @action
   selectTab(view) {
     if (view.get("isLink")) {
       this.triggerAction(view.get("action"));
@@ -16,22 +19,28 @@ export default class TabbedSection extends Component {
     if (selected) {
       selected.set("active", false);
     }
+
     this.set("selected", view);
     view.set("active", true);
+
     this.onTabChange(view.name);
   }
 
+  @action
   addTab(tab) {
     this.tabs.addObject(tab);
+
     if (!this.selected && !tab.get("isLink")) {
       this.selectTab(tab);
     }
   }
 
+  @action
   removeTab(tab) {
     if (this.selected === tab) {
       this.set("selected", null);
     }
+
     this.tabs.removeObject(tab);
   }
 }
