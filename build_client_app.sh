8d9c30fcd50f2e815f16a99e455fc65b1a7b22b6
Jarek Radosz <jradosz@gmail.com>
2023-03-04 11:17:29 +0100

    DEV: Update the build process

--- build_client_app.sh
+++ build_client_app.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-(cd client-app && npm i && npx ember build --environment=${1:-production})
+(cd client-app && yarn && yarn ember build --environment=${1:-production})
 
-rm assets/javascript/*
-rm assets/stylesheets/client-app.css
-rm assets/stylesheets/vendor.css
+rm -f assets/javascript/*
+rm -f assets/stylesheets/client-app.css
+rm -f assets/stylesheets/vendor.css
 
-cp client-app/dist/assets/client-app.js assets/javascript/client-app.js
-cp client-app/dist/assets/vendor.js assets/javascript/vendor.js
-cp client-app/dist/assets/client-app.css assets/stylesheets/client-app.css
-cp client-app/dist/assets/vendor.css assets/stylesheets/vendor.css
+cp client-app/dist/assets/*.js assets/javascript/
+cp client-app/dist/assets/*.css assets/stylesheets/
